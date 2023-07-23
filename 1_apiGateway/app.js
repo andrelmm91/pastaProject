8174375dca901eaededa971678b539d9f5b1fc9b
andrelmm91 <andre.lmm91@gmail.com>
2023-07-23 21:35:53 +0200

    initial git

---
+++ 1_apiGateway/app.js
@@ -0,0 +1,28 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const authRouter = require("./routes/authRouter");
+
+const app = express();
+
+app.use(bodyParser.json()); // body parser receving data
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// // routing
+app.use("/auth", authRouter);
+
+// // handlers
+// Error handler
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong.";
+  res.status(status).json({ message: message });
+});
+
+app.listen(8080);
