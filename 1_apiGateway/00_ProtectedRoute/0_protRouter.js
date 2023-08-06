48e9d1a8a05f1a9b5c4df590ed96afec021e94b7
andrelmm91 <andre.lmm91@gmail.com>
2023-08-06 22:18:54 +0200

    protroute auth

--- 1_apiGateway/00_ProtectedRoute/0_protRouter.js
+++ 1_apiGateway/00_ProtectedRoute/0_protRouter.js
@@ -4,10 +4,10 @@ const { validateToken } = require("../01_authRouters/tokenHandler");
 module.exports = async (req, res, next) => {
   console.log("I passed here auth");
   try {
-    // const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = await validateToken(req.headers.authorization);
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = await validateToken(token);
     const userEmail = decodedToken.email;
-    console.log("I passed here auth 2", req.headers.authorization);
+    console.log("I passed here auth 2", token);
     console.log("I passed here auth 2", userEmail, req.body.email);
     console.log("I passed here auth 2", decodedToken);
 
